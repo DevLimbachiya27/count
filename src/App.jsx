ec3f83f8a92449afb61de7426e9edcef4da97b91
Dev Limbachiya <limbachiyadev35@gmail.com>
2025-11-29 17:33:30 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Timer from './components/timer'
+import Counter from './components/counter'
+
+function App() {
+
+  return (
+    <>
+        <Timer/>
+        <Counter/>
+    </>
+  )
+}
+
+export default App
